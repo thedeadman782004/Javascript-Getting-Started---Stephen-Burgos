7c008dc4cb701752b6e0b60fd434346cabb4d131
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-09 09:59:16 -0500

    Tried to use import/export, but couldn't figure it out in vanilla JS.  Undid changes.

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function sendOut(arg){
     document.write(" ");
     document.write(arg);
 }
-class numberObject{
+class NumberObject {
     constructor(num1 = 1, num2 = 2, num3 = 3){
         this.num1 = num1;
         this.num2 = num2;
@@ -15,8 +15,9 @@ class numberObject{
     get getNum1(){return this.num1;}
     get getNum2(){return this.num2;}
     get getNum3(){return this.num3;}
+    getArray(){return [this.num1,this.num2,this.num3];}
+    print(){console.log(this);}
 }
-
 sendOut("Hello, world!");
 const [a,b,c] = [1,2,3];
 sendOut(`${a} ${b} ${c}`);
@@ -27,5 +28,11 @@ catch (error){
     console.log(error);
 }
 
-const myNumbers = new numberObject(5,9,15);
-console.log(`myNumbers objects: ${myNumbers.getNum1}, ${myNumbers.getNum2}, ${myNumbers.getNum3}.`);
+const myNumbers = new NumberObject(5,9,15);
+console.log(`myNumbers object: ${myNumbers.getNum1}, ${myNumbers.getNum2}, ${myNumbers.getNum3}.`);
+console.log(myNumbers.print());
+
+const myArray = myNumbers.getArray();
+console.log(`myArray = ${myArray}. Splicing...`);
+myArray.splice(1,1,76,901,89,765,4);
+console.log(`New array: ${myArray}.`);
