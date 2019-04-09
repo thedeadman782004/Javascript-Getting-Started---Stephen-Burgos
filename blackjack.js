f8e6dc07c6e5298df5d7cb859b70e6f43e5fbb6e
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-09 13:12:15 -0500

    Finished the blackjack game.

--- blackjack.js
+++ blackjack.js
@@ -64,10 +64,7 @@ function showStatus() {
 function shuffleDeck(deck) {
     for (let i = 0; i < deck.length; i++){
         let swapIdx = Math.trunc(Math.random() * deck.length);
-        let tmp = deck[swapIdx];
-        deck[swapIdx] = deck[i];
-        deck[i] = tmp;
-        // deck[swapIdx,i] = deck[i,swapIdx];
+        deck[swapIdx,i] = deck[i,swapIdx];
     }
 }
 
@@ -107,6 +104,43 @@ function getCardNumericValue(card){
     }
 } 
 
+function checkForEndOfGame(){
+    updateScores();
+
+    if (gameOver){
+        //let dealer take cards
+        while(dealerScore <= playerScore && playerScore <= 21 && dealerScore <= 21){
+            dealerCards.push(getNextCard());
+            updateScores();
+        }
+    }
+    console.log(dealerScore);
+    if (playerScore > 21){
+        playerWon = false;
+        gameOver = true;
+    }
+    else if (dealerScore > 21){
+        playerWon = true;
+        gameOver = true;
+    }
+    else if (playerScore === 21){
+        playerWon = true;
+        gameOver = true;
+    }
+    else if (playerScore === dealerScore){
+        playerWon = true;
+        gameOver = true;
+    }
+    else if (gameOver){
+        if(playerScore >= dealerScore){
+            playerWon = true;
+        }
+        else {
+            playerWon = false;
+        }
+    }
+}
+
 /**** SETTING UP VARIABLES */
     // Arrays for building the deck
     let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
@@ -141,5 +175,17 @@ newGameButton.addEventListener('click', function(){
     newGameButton.style.display = 'none';
     hitButton.style.display = 'inline';
     stayButton.style.display = 'inline';
+    showStatus();
+});
+
+hitButton.addEventListener('click', function() {
+    playerCards.push(getNextCard());
+    checkForEndOfGame();
+    showStatus();
+});
+
+stayButton.addEventListener('click', function() {
+    gameOver = true;
+    checkForEndOfGame();
     showStatus();
 });
