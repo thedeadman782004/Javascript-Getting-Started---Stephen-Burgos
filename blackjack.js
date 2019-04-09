b7bb96fad05e07a434c55c8fa53d67ed5c7764de
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-09 12:00:22 -0500

    Added functionality for printing the score to the page, updating the score and other necessary functions.

--- blackjack.js
+++ blackjack.js
@@ -4,7 +4,7 @@
 // Source: Pluralsight
 
 // Function to build the deck from card objects
-function createDeck(){
+function createDeck() {
     let deck = [];
     for (let suitIdx = 0; suitIdx < suits.length; suitIdx++){
         for (let valueIdx = 0; valueIdx < values.length; valueIdx++){
@@ -18,41 +18,128 @@ function createDeck(){
     return deck;
 }
 
-// Get a string from the card object that can be printed to the console.
+// Shift one card from the front of the deck and return that card
+function getNextCard() {
+    return deck.shift();
+}
+
 function getCardString(card){
     return card.value + " of " + card.suit;
 }
 
-// Shift one card from the front of the deck and return that card
-function getNextCard(){
-    return deck.shift();
+function showStatus() {
+    if (!gameStarted){
+        textArea.innerText = "Welcome to Blackjack!";
+        return;
+    }
+
+    // Turn things into strings
+    let dealerCardString = "";
+    for (let i = 0; i < dealerCards.length; i++){
+        dealerCardString += getCardString(dealerCards[i]) + "\n";
+    }
+    let playerCardString = "";
+    for (let i = 0; i < playerCards.length; i++){
+        playerCardString += getCardString(playerCards[i]) + "\n";
+    }
+
+    updateScores();
+
+    textArea.innerText = "Dealer has:\n" + dealerCardString + "(score: " + dealerScore + ")\n\n" + 
+                         "Player has:\n" + playerCardString + "(score: " + playerScore + ")\n\n"
+    
+    if (gameOver){
+        if(playerWon){
+            textArea.innerText += "YOU WIN!";
+        }
+        else{
+            textArea.innerText += "DEALER WINS";
+        }
+        newGameButton.style.display = "inline";
+        hitButton.style.display = "none";
+        stayButton.style.display = "none";''
+    }
 }
 
-// Arrays for building the deck
-let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
-let values = ["Ace","King","Queen","Jack","Ten","Nine","Eight","Seven","Six","Five","Four","Three","Two"];
-let deck = createDeck();
+function shuffleDeck(deck) {
+    for (let i = 0; i < deck.length; i++){
+        let swapIdx = Math.trunc(Math.random() * deck.length);
+        let tmp = deck[swapIdx];
+        deck[swapIdx] = deck[i];
+        deck[i] = tmp;
+        // deck[swapIdx,i] = deck[i,swapIdx];
+    }
+}
 
-// Deal cards to the player
-let playerCards = [ getNextCard(),getNextCard() ];
+function updateScores(){
+    dealerScore = getScore(dealerCards);
+    playerScore = getScore(playerCards);
+}
 
-// Log messages
-console.log("Welcome to Blackjack!");
-console.log("You are dealt: ");
-console.log("   " + getCardString(playerCards[0]));
-console.log("   " + getCardString(playerCards[1]));
+function getScore(cardArray){
+    let score = 0;
+    let hasAce = false;
+    for (let i = 0; i < cardArray.length; i++){
+        let card = cardArray[i];
+        score += getCardNumericValue(card);
+        if (card.value === "Ace"){
+            hasAce = true;
+        }
+    }
+    if (hasAce && score + 10 <= 21){
+        return score + 10;
+    }
+    return score;
+}
 
-// Get elements from blackjack.html
-let newGameButton = document.getElementById("new-game-button");
-let textArea = document.getElementById("text-area");
-let hitButton = document.getElementById("hit-button");
-let stayButton = document.getElementById("stay-button");
+function getCardNumericValue(card){
+    switch(card.value){
+        case "Ace": return 1;
+        case "Two": return 2;
+        case "Three": return 3;
+        case "Four": return 4;
+        case "Five": return 5;
+        case "Six": return 6;
+        case "Seven": return 7;
+        case "Eight": return 8;
+        case "Nine": return 9;
+        default: return 10;
+    }
+} 
+
+/**** SETTING UP VARIABLES */
+    // Arrays for building the deck
+    let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+    let values = ["Ace","King","Queen","Jack","Ten","Nine","Eight","Seven","Six","Five","Four","Three","Two"];
+
+    // Get elements from blackjack.html
+    let newGameButton = document.getElementById("new-game-button");
+    let textArea = document.getElementById("text-area");
+    let hitButton = document.getElementById("hit-button");
+    let stayButton = document.getElementById("stay-button");
+
+    // Game variables
+    let [gameStarted, gameOver, playerWon, dealerCards, playerCards, dealerScore, playerScore, deck] = [false, false, false, [], [], 0, 0, []];
+/**** */
+
+// Hide buttons
 hitButton.style.display = 'none';
 stayButton.style.display = 'none';
+showStatus();
 
+// Start a game
 newGameButton.addEventListener('click', function(){
-    textArea = "Started";
+    gameStarted = true;
+    gameOver = false;
+    playerWon = false;
+
+    deck = createDeck();
+    shuffleDeck(deck);
+    dealerCards = [ getNextCard(), getNextCard() ];
+    playerCards = [ getNextCard(), getNextCard() ];
+
     newGameButton.style.display = 'none';
     hitButton.style.display = 'inline';
     stayButton.style.display = 'inline';
+    showStatus();
 });
